9e73111137737a4629e91de6cfc688b9a2ddfa50
Eugenia <eugeniasol13@gmail.com>
2020-11-17 17:56:54 -0500

    deploying

---
+++ employee-directory/build/precache-manifest.87878ca290800e8a4aab57b1fee131f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b15f5f38d1a096053f0175a049c256",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "9e18305cbf5e7140e6a1",
+    "url": "/employee-directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "10648fd9f6f5372bf3e3",
+    "url": "/employee-directory/static/css/main.3bd4fe0b.chunk.css"
+  },
+  {
+    "revision": "9e18305cbf5e7140e6a1",
+    "url": "/employee-directory/static/js/2.042ec11a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/employee-directory/static/js/2.042ec11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10648fd9f6f5372bf3e3",
+    "url": "/employee-directory/static/js/main.13847778.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
